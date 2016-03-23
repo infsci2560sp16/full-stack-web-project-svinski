98e2b30d879d541f026d13c5cff0860c6af4fabe
Sarah <svinski23@gmail.com>
2016-03-23 09:27:35 -0400

    made get("/"... active

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
 
     get("/hello", (req, res) -> "Hello World");
 
-   /* get("/", (request, response) -> {
+   get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
           attributes.put("message", "Hello World!");
 
