c625d07c3aefb4cf5a127e549c646abb4d215913
Sarah <svinski23@gmail.com>
2016-04-01 15:35:31 -0400

    main updates

--- src/main/java/AddUserController.java
+++ src/main/java/AddUserController.java
@@ -41,8 +41,7 @@ public class AddUserController {
                 userData.put("website", website);
                 userData.put("craft", craft);
                 userData.put("about", about);
-                Gson gson = new Gson();
-                
+                Gson gson = new Gson();                
                 return gson.toJson(userData);
             });
         
--- src/main/java/Routes/ftlRoutes.java
+++ src/main/java/Routes/ftlRoutes.java
@@ -3,6 +3,7 @@ package Routes;
 import com.google.gson.Gson;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import spark.ModelAndView;
 import static spark.Spark.get;
 import spark.template.freemarker.FreeMarkerEngine;
@@ -59,7 +60,30 @@ public class ftlRoutes {
                 attributes.put("user", "Summer");
                 
                 return new ModelAndView(attributes, "header.ftl");
-            }, new FreeMarkerEngine());                
+            }, new FreeMarkerEngine());  
+    get("/newusers", (req, res) -> {
+                String username = req.queryParams("username");
+            	String name = req.queryParams("name");
+                String email  = req.queryParams("email");
+                String bizname = req.queryParams("bizname");
+                String website = req.queryParams("website");
+                String craft = req.queryParams("craft");
+                String about = req.queryParams("about");
+            	String id = UUID.randomUUID().toString();
+                
+                Map<String, Object> userData = new HashMap<>();
+                userData.put("id", id);
+                userData.put("username", username);
+                userData.put("name", name);
+                userData.put("email", email);
+                userData.put("bizname", bizname);
+                userData.put("website", website);
+                userData.put("craft", craft);
+                userData.put("about", about);
+                Gson gson = new Gson();
+                
+                return gson.toJson(userData);
+            });
                 
     
         }   
